ac51d291640d4ac2f043d331d2e585e59ec9b2d3
Yavuz Çelik <63187562+y4wwy@users.noreply.github.com>
2023-08-07 14:10:16 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Layout/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Fragment>
+      <Header />;
+    </Fragment>
+  );
 };
 
 export default App;
