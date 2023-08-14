5175bcf41711feb053a3a7ae5f73a5a30672d99a
Yavuz Çelik <63187562+y4wwy@users.noreply.github.com>
2023-08-14 03:46:07 +0300

    ...

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,7 +11,7 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     let updatedItems;
     //const updatedItems = state.items.concat(action.item);
-    const uptadedTotalAmount =
+    const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
@@ -31,7 +31,26 @@ const cartReducer = (state, action) => {
 
     return {
       items: updatedItems,
-      totalAmount: uptadedTotalAmount,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
